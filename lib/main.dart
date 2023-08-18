32c7cabf977eea26693d0f9459bdc2ba9de0374b
Trixie Diane Bautista <tbautista@diavox.net>
2023-08-18 14:13:43 +0800

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+//build method - returns a widget, and will be called every time flutter needs to rebuild the UI when your data changes
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.pink,
+            title: const Text('My first app!'),
+          ),
+          // body: const Center(
+          //   child: SizedBox(
+          //     child: Text('Hello, Diane!'),
+          //     height: 50,
+          //     width: 50,
+          //   ),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Icon(Icons.backpack),
+              Icon(Icons.leaderboard),
+              Icon(Icons.person)
+            ],
+          )),
+    );
+  }
+}
